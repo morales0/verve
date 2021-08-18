672d944729fffbc3a44fb8c61f43b66a05f2fdf6
Francisco Morales <chico.fmorales@gmail.com>
2021-08-17 20:47:08 -0400

    History is functioning

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,9 +1,11 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components/macro";
 import { flexRow, flexCol, flexCenter } from "layout";
 import { useDatabase } from "reactfire";
 import { useAuthCheck } from "context/auth";
 import { useHistory } from "hooks/history";
 import { Link } from "react-router-dom";
+import { Button, PageHeader } from "components";
 
 // Styled components
 const HistoryContainer = styled.div`
@@ -28,6 +30,8 @@ const HistoryContainer = styled.div`
 
       .workoutListDay_container {
          display: flex;
+         padding: .75rem 0;
+         overflow-x: auto;
       }
    }
 
@@ -42,34 +46,108 @@ const WorkoutDisplayContainer = styled.div`
    display: flex;
    flex-direction: column;
    padding: .5rem;
-   border: 1px solid #555;
-
-   width: 250px;
-   height: 325px;
+   width: 200px;
+   height: 300px;
+   background: #f3e8b1;
+   border: 1px solid #b5b5b5;
+   border-radius: 3px;
 
    & + & {
       margin-left: 1rem;
    }
+
+   & > .date {
+      display: flex;
+      justify-content: flex-end;
+
+      margin-top: auto;
+   }
 `
 
+const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // History component
 const History = (props) => {
    const workoutHistory = useHistory()
+   const [militaryTime, setMilitaryTime] = useState(false);
+   const [listOfDays, setListOfDays] = useState([]);
+   const [workoutsByDay, setWorkoutsByDay] = useState({});
+
+   // Once, on mount, create array of days from today
+   useEffect(() => {
+      const now = new Date()
+      const days = []
+      for (let index = 0; index < 6; index++) {
+         let nextDate = new Date();
+         nextDate.setDate(now.getDate() - index);
+         days.push(nextDate)
+      }
+
+      setListOfDays(days)
+   }, []);
+
+   // Construct workout by day map when history changes
+   useEffect(() => {
+      if (workoutHistory.data !== null) {
+         let newWorkoutsByDay = {}
+
+         // Create json
+         Object.values(workoutHistory.data).reverse().forEach(workout => {
+            // Index by string describing unique day
+            const workoutDate = new Date(workout.dateStarted)
+            const workoutKey = toDateKey(workoutDate)
+
+            // Push to array, make new one if necessary
+            if (workoutKey in newWorkoutsByDay) {
+               newWorkoutsByDay[workoutKey].push(workout)
+            } else {
+               newWorkoutsByDay[workoutKey] = [workout]
+            }
+         })
+
+         setWorkoutsByDay(newWorkoutsByDay)
+      }
+   }, [workoutHistory.data]);
 
    // Functions
-   const isDateToday = (date) => {
-      const now = new Date()
 
-      return now.getDate() === date.getDate() &&
-         now.getDay() === date.getDay() &&
-         now.getMonth() === date.getMonth() &&
-         now.getFullYear() === date.getFullYear()
+   const toDateKey = (date) => {
+      const day = dayOfWeek[date.getDay()]
+      const dateNum = date.getDate()
+      const month = date.getMonth()
+      const year = date.getFullYear()
+
+      return `${day}, ${month}-${dateNum}-${year}`
+   }
+
+   const toTimeString = (date) => {
+      const hour = date.getHours()
+      const min = date.getMinutes()
+
+      return `${!militaryTime && hour > 12 ? hour % 12 : hour}:${min < 10 ? '0' + min : min}${militaryTime ? '' : hour > 12 ? ' PM' : ' AM'}`
+   }
+
+   const areSameDay = (date1, date2) => {
+      return date1.getDate() === date2.getDate() &&
+         date1.getDay() === date2.getDay() &&
+         date1.getMonth() === date2.getMonth() &&
+         date1.getFullYear() === date2.getFullYear()
    }
 
    // Turn dates into strings like "today", "yesterday", etc
    const dateToDetailString = (date) => {
+      const today = new Date()
+      const yesterday = new Date()
+      yesterday.setDate(today.getDate() - 1)
 
+      if (areSameDay(today, date)) {
+         return "Today"
+      } else if (areSameDay(yesterday, date)) {
+         return "Yesterday"
+      } else {
+         const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+         return date.toLocaleDateString("en-US", options)
+      }
    }
 
    const orgByDay = (workouts) => {
@@ -101,64 +179,53 @@ const History = (props) => {
    return (
       <HistoryContainer>
          <header css={`${flexRow}`}>
-            Header
+            <PageHeader title="History" />
          </header>
 
-         {
-            workoutHistory.data ? (
-               <div className="historyMainContent_container">
-                  {
-                     Object.entries(orgByDay(Object.values(workoutHistory.data))).map(([day, workoutList]) => (
-                        <div className="historyDay_container">
-                           <h2>{day}</h2>
+         <div className="historyMainContent_container">
+            {
+               listOfDays.map((day, i) => (
+                  <div className="historyDay_container">
+                     <h2>
+                        {dateToDetailString(day)}
+                     </h2>
+                     {
+                        workoutHistory.status === "loading" ? (
+                           <div>
+                              Loading...
+                           </div>
+                        ) : (
                            <div className="workoutListDay_container">
                               {
-                                 workoutList.map(workout => (
-                                    <WorkoutDisplayContainer>
-                                       <div>
-                                          <input type="text" placeholder="We will use this to filter"/>
-                                       </div>
-                                       <div>
-                                          {Object.keys(workout.exercises).map(ex => <h4>{ex}</h4>)}
-                                       </div>
-                                    </WorkoutDisplayContainer>
-                                 ))
+                                 workoutsByDay[toDateKey(day)] ? (
+                                    Object.values(workoutsByDay[toDateKey(day)])
+                                    .map(workout => (
+                                       <WorkoutDisplayContainer>
+                                          <div className="searchFilter_container">
+                                             <input type="text" placeholder="We will use this to filter" />
+                                          </div>
+                                          <div className="exercises_container">
+                                             {Object.keys(workout.exercises).map(ex => <h4>{ex}</h4>)}
+                                          </div>
+                                          <div className="date">
+                                             {toTimeString(new Date(workout.dateStarted))}
+                                          </div>
+                                       </WorkoutDisplayContainer>
+                                    ))
+                                 ) : (
+                                    "No workouts"
+                                 )
                               }
                            </div>
-                        </div>
-                     ))
-                  }
-               </div>
-            ) : (
-               <div>Everybody starts somewhere. Go on and <Link to="/workout">make history!</Link></div>
-            )
-         }
-
-         {/* <div className="historyMainContent_container">
-            <div className="historyDay_container">
-               <h2>Today</h2>
-               <div className="workoutListDay_container">
-                  <WorkoutDisplayContainer>Workout 1</WorkoutDisplayContainer>
-                  <WorkoutDisplayContainer>Workout 2</WorkoutDisplayContainer>
-               </div>
-            </div>
-
-            <div className="historyDay_container">
-               <h2>Yesterday</h2>
-               <div className="workoutListDay_container">
-                  <WorkoutDisplayContainer>Workout 1</WorkoutDisplayContainer>
-                  <WorkoutDisplayContainer>Workout 2</WorkoutDisplayContainer>
-               </div>
-            </div>
-
-            <div className="historyDay_container">
-               <h2>July 28</h2>
-               <div className="workoutListDay_container">
-                  <WorkoutDisplayContainer>Workout 1</WorkoutDisplayContainer>
-                  <WorkoutDisplayContainer>Workout 2</WorkoutDisplayContainer>
-               </div>
-            </div>
-         </div> */}
+                        )
+                     }
+                  </div>
+               ))
+            }
+            <Button>
+               Load more...
+            </Button>
+         </div>
       </HistoryContainer>
    );
 }
