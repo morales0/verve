55b8ec8163d8d1f8ff6c2367c26600da9e6aa754
Francisco Morales <chico.fmorales@gmail.com>
2023-12-10 20:37:18 -0800

    start button

--- src/pages/Home/main-view/start-button/start-button.tsx
+++ src/pages/Home/main-view/start-button/start-button.tsx
@@ -1,12 +1,56 @@
-import { Button, Text } from "@mantine/core";
+import { useUser } from "@/context/user";
+import { Icon } from "@iconify/react";
+import { Button, Group, Text } from "@mantine/core";
+import { DatabaseReference, child, set } from "firebase/database";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const createWorkout = async (userRef: DatabaseReference) => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return set(child(userRef, "/currentWorkout"), {
+    dateStarted: now.toString(),
+    timeStarted: time,
+  }).then(() => {
+    set(child(userRef, "/meta/isWorkingOut"), true);
+  });
+};
+
 export const StartButton = () => {
+  const { dataRef, meta } = useUser();
+  const [starting, setStarting] = useState(false);
   const navigate = useNavigate();
 
+  const startWorkout = () => {
+    setStarting(true);
+
+    if (meta.isWorkingOut) {
+      navigate("/workout");
+      setStarting(false);
+      return;
+    }
+
+    createWorkout(dataRef).then(() => {
+      navigate("/workout");
+      setStarting(false);
+    });
+  };
+
   return (
-    <Button variant="gradient" gradient={{ from: "teal", to: "blue", deg: 120 }} onClick={() => navigate("/workout")}>
-      <Text>Start Workout</Text>
+    <Button
+      loading={starting}
+      variant="gradient"
+      gradient={{ from: "teal", to: "blue", deg: 120 }}
+      onClick={startWorkout}
+    >
+      <Group gap="xs">
+        <Icon icon="bi:fire" />
+        <Text>{meta.isWorkingOut ? "Continue Workout" : "Start Workout"}</Text>
+      </Group>
     </Button>
   );
 };
